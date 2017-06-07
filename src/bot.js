687642f23a4a1d12d13b8e900e18faa3248b81ee
davy42 <davydov_alexander@bk.ru>
2017-06-07 18:31:50 +0300

    test2

--- src/bot.js
+++ src/bot.js
@@ -36,86 +36,58 @@ function onCommand(session, command) {
     case 'ico':
       ico(session)
       break
-    case 'count':
-      count(session)
-      break
-    case 'donate':
-      donate(session)
-      break
   }
 }
 
-function onPayment(session, message) {
-  if (message.fromAddress == session.config.paymentAddress) {
-    // handle payments sent by the bot
-    if (message.status == 'confirmed') {
-      // perform special action once the payment has been confirmed
-      // on the network
-    } else if (message.status == 'error') {
-      // oops, something went wrong with a payment we tried to send!
-    }
-  } else {
-    // handle payments sent to the bot
-    if (message.status == 'unconfirmed') {
-      // payment has been sent to the ethereum network, but is not yet confirmed
-      sendMessage(session, `Thanks for the payment! 🙏`);
-    } else if (message.status == 'confirmed') {
-      // handle when the payment is actually confirmed!
-    } else if (message.status == 'error') {
-      sendMessage(session, `There was an error with your payment!🚫`);
-    }
-  }
-}
+
 
 // STATES
 
 function welcome(session) {
-  sendMessage(session, `Hello Token!`)
+  sendFirstMessage(session, `Hello! I know about all ICO's!`)
 }
 
-function pong(session) {
-  sendMessage(session, `Pong azaza`)
-}
 
 function ico(session) {
-  req('http://chaingear.cyber.fund/chaingear.json')
-    .then((body) => {
-      var ico = body[1].ico;
-      sendMessage(session, ico)
-    })
+      showICO(session)
 }
 
-// example of how to store state on each user
-function count(session) {
-  let count = (session.get('count') || 0) + 1
-  session.set('count', count)
-  sendMessage(session, `${count}`)
-}
 
-function donate(session) {
-  // request $1 USD at current exchange rates
-  Fiat.fetch().then((toEth) => {
-    session.requestEth(toEth.USD(1))
-  })
-}
+
+
 
 // HELPERS
 
-function sendMessage(session, message) {
-  let controls = [{
+// function sendMessage(session, message) {
+//   let controls = [{
+//       type: 'button',
+//       label: 'ico',
+//       value: 'ico'
+//     },
+//     {
+//       type: 'button',
+//       label: 'Count',
+//       value: 'count'
+//     },
+//     {
+//       type: 'button',
+//       label: 'Donate',
+//       value: 'donate'
+//     }
+//   ]
+//   session.reply(SOFA.Message({
+//     body: message,
+//     controls: controls,
+//     showKeyboard: false,
+//   }))
+// }
+
+function sendFirstMessage(session, message) {
+  let controls = [
+    {
       type: 'button',
-      label: 'ico',
+      label: 'ICO',
       value: 'ico'
-    },
-    {
-      type: 'button',
-      label: 'Count',
-      value: 'count'
-    },
-    {
-      type: 'button',
-      label: 'Donate',
-      value: 'donate'
     }
   ]
   session.reply(SOFA.Message({
@@ -124,3 +96,30 @@ function sendMessage(session, message) {
     showKeyboard: false,
   }))
 }
+
+function showICO(session){
+  session.reply(SOFA.Message({
+  body: "What would you like to do next?",
+  controls: [
+    {
+      type: "group",
+      label: "Trip",
+      controls: [
+        {type: "button", label: "Directions", value: "directions"},
+        {type: "button", label: "Timetable", value: "timetable"},
+        {type: "button", label: "Exit Info", value: "exit"},
+        {type: "button", label: "Service Conditions", value: "conditions"}
+      ]
+    },
+    {
+      type: "group",
+      label: "Services",
+      "controls": [
+        {type: "button", label: "Buy Ticket", value: "buy-ticket"},
+        {type: "button", label: "Support", value: "support"}
+      ]
+    }
+  ]
+}))
+}
+
